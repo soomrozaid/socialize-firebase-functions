75621cef09e8cb6a4ef785f3231266b9aca07d40
Zaid Soomro <soomrozaid@gmail.com>
2020-09-14 02:10:52 -0600

    commit

---
+++ dbschema.js
@@ -0,0 +1,16 @@
+let db = {
+  // users: [
+  //   {
+  //     userId:
+  //   }
+  // ],
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-09-05T04:07:32.040Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAx-iR83Cf-NzXcnQ0Gvp7PVdPSL2DP6BY",
+  authDomain: "socialize-70b85.firebaseapp.com",
+  databaseURL: "https://socialize-70b85.firebaseio.com",
+  projectId: "socialize-70b85",
+  storageBucket: "socialize-70b85.appspot.com",
+  messagingSenderId: "187332942635",
+  appId: "1:187332942635:web:e725da9db3cee1123a836e",
+  measurementId: "G-9ZVRK8Z4K3",
+};
